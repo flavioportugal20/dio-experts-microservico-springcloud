5785d60a681186df49ea36bdfe783c57d2872edf
unknown <flavioportugal20@gmail.com>
2021-07-31 15:16:16 -0300

    Enviando projeto

---
+++ config-server/src/test/java/com/dio/microservico/springcloud/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.microservico.springcloud.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
