f42a437c5bd67be02c56d6789e30f58245de06d1
unknown <flavioportugal20@gmail.com>
2021-08-01 18:03:21 -0300

    Add gateway

---
+++ gateway/src/main/java/com/dio/microservico/springcloud/gateway/GatewayApplication.java
@@ -0,0 +1,13 @@
+package com.dio.microservico.springcloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
